353c6813ca2a5d91d6d79768492769546211daf5
Goubert Rodrigues <goubert@gmail.com>
2023-06-04 09:12:44 -0300

    menu e outras coisas

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,49 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {headerFix()};
 
+// Get the header
+var header = document.getElementById("header");
 
-$(function(){
-  'use strict';
-  var $page = $('#main'),
-      options = {
-        debug: true,
-        prefetch: true,
-        cacheLength: 2,
-        onStart: {
-          duration: 250, // Duration of our animation
-          render: function ($container) {
-            // Add your CSS animation reversing class
-            $container.addClass('is-exiting');
-            // Restart your animation
-            smoothState.restartCSSAnimations();
-          }
-        },
-        onReady: {
-          duration: 0,
-          render: function ($container, $newContent) {
-            // Remove your CSS animation reversing class
-            $container.removeClass('is-exiting');
-            // Inject the new content
-            $container.html($newContent);
-          }
-        }
-      },
-      smoothState = $page.smoothState(options).data('smoothState');
-});
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
 
-$(window).scroll(function(){
-  if ($(window).scrollTop() >= 100) {
-      $('header').addClass('fixed-header');
-      $('header ul').addClass('fixed-menu');
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function headerFix() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
   }
-  else {
-      $('header').removeClass('fixed-header');
-      $('header ul').addClass('fixed-menu');
-  }
-});
+}
 
+// Get all sections that have an ID defined
+const sections = document.querySelectorAll("section[id]");
 
+// Add an event listener listening for scroll
+window.addEventListener("scroll", navHighlighter);
 
+function navHighlighter() {
+  
+  // Get current scroll position
+  let scrollY = window.pageYOffset;
+  
+  // Now we loop through sections to get height, top and ID values for each
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 500;
+    sectionId = current.getAttribute("id");
+    
+    /*
+    - If our current scroll position enters the space where current section on screen is, add .active class to corresponding navigation link, else remove it
+    - To know which link needs an active class, we use sectionId variable we are getting while looping through sections as an selector
+    */
+    if (
+      scrollY > sectionTop &&
+      scrollY <= sectionTop + sectionHeight
+    ){
+      document.querySelector(".main-menu li a[href*=" + sectionId + "]").classList.add("active");
+    } else {
+      document.querySelector(".main-menu li a[href*=" + sectionId + "]").classList.remove("active");
+    }
+  });
+}
